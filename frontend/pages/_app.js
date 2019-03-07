f39593849b65fc35ef3f46a944626e48b5ae7e99
Zachary Zhou <avengyy@gmail.com>
2019-03-07 17:25:26 +0800

    Fix compiling error with graphql-anywhere dependency

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -21,7 +21,7 @@ class MyApp extends App {
       <Container>
         <ApolloProvider client={apollo}>
           <Page>
-            <Component />
+            <Component {...pageProps} />
           </Page>
         </ApolloProvider>
       </Container>
